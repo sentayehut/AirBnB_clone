fe221f3bf8cf5c7cf58234869015f800445825e5
sentayehut <tsentayehu4@gmail.com>
2022-01-29 04:10:56 -0800

    base mode

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,49 @@
 #!/usr/bin/python3
-"""This is the Base Model module.
-Contains the BaseModel class which will be the
-"base" of all other classes in this project.
-"""
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
+from datetime import datetime
 
 
-class BaseModel():
-    """This class will be the “base” of all other classes in this project.
-    The goal is to manage all common attributes and methods for other classes.
-    Attributes:
-        id (str): unique random ID for each BaseModel instance.
-        created_at (datetime): the current datetime when instance is created.
-        updated_at (datetime): the current datetime when instance is updated.
-    """
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initializes the default attributes of the BaseModel object.
+        """Initialize a new BaseModel.
         Args:
-            *args: unused.
-            **kwargs (dict): a dictionary containing wanted attributes.
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwargs is not None and kwargs != {}:
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k in ["created_at", "updated_at"]:
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k != "__class__":
-                    setattr(self, k, v)
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
-    def __str__(self):
-        """Overrides the default behaviour of the __str__ method."""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary with all the keys/values of the instance."""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
